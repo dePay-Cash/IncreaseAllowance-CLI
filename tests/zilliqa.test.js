843b06c330ff9d03c97b251b5b61edf5d69e85d8
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-08-23 16:47:55 +0200

    tests jest

---
+++ tests/zilliqa.test.js
@@ -0,0 +1,16 @@
+"use strict";
+const increaseFTAllowance = require("../src/zilliqa");
+
+/*
+payload = {
+    ftContractAddress: <ftContractAddress>, --> type: string
+    ftContractOwnerPrivateKey: <ftContractOwnerPrivateKey>, --> type: string
+    spender: <accountAddressToBeAllowed>, --> type: string
+    amount: <amountOfFTToBeAllowed> --> type: string
+}
+*/
+
+test("test call Api ", async () => {
+  const response = await increaseFTAllowance();
+  expect(response).toStrictEqual({});
+});
