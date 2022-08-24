73e54256011f8906da2e7384b9bbcf3661e0c845
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-08-24 09:18:05 +0200

    run IIFE main

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const payload = require("minimist")(process.argv.slice(2));
 const increaseFTAllowance = require("./src/zilliqa");
 
-async function main() {
+(async () => {
   try {
     await increaseFTAllowance(payload);
   } catch (err) {
     console.error(err);
   }
-}
+})();
